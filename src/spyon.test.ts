d44e50616add18b7235e23164ea9be313b1038a0
O Je-Gwan <emperorvv@gmail.com>
2021-05-22 12:05:28 +0900

    chore: 주석 제거

--- src/spyon.test.ts
+++ src/spyon.test.ts
@@ -12,19 +12,6 @@ afterEach(() => {
   data.users.splice(0);
 });
 
-// test("findOne returns what axios get returns", async () => {
-//   axios.get = jest.fn().mockResolvedValue({
-//     data: {
-//       id: 1,
-//       name: "Dale Seo",
-//     },
-//   });
-
-//   const user = await userService.findOne(1);
-//   expect(user).toHaveProperty("id", 1);
-//   expect(user).toHaveProperty("name", "Dale Seo");
-// });
-
 test("spyon", () => {
   const calculator = {
     add: (a: number, b: number) => a + b,
